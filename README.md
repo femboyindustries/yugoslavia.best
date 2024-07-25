1849eb2c1f352683766cfafbf9fd113a625715ba
numpad_7 <bestie@mayf.pink>
2024-07-25 20:43:04 +0200

    optimize

--- README.md
+++
@@ -1,1 +0,0 @@
-https://github.com/femboyindustries/yugoslavia.best
--- index.php
+++
@@ -1,4 +0,0 @@
-<? if (!function_exists('fetch_yugoslavia') || !getenv('john')){
-    echo "you need to compile and install <a href='https://git.oat.zone/mayflower/john'>the john extension</a>";
-}else{echo fetch_yugoslavia();} ?>
-
