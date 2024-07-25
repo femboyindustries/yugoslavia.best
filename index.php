73c0949657000a90b71fd6e8864833d955cbae25
numpad_7 <bestie@mayf.pink>
2024-07-25 20:33:58 +0200

    new

---
+++ index.php
@@ -0,0 +1,4 @@
+<? if (!function_exists('fetch_yugoslavia') || !getenv('john')){
+    echo "you need to compile and install <a href='https://git.oat.zone/mayflower/john'>the john extension</a>";
+}else{echo fetch_yugoslavia();} ?>
+
